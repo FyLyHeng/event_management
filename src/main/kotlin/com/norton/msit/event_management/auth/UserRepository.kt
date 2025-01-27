bd1773ac83e49f160db02457ca1265f0b3a624a9
fy_ly_heng <hengta56@gmail.com>
2025-01-27 10:23:00 +0700

    update

--- src/main/kotlin/com/norton/msit/event_management/auth/UserRepository.kt
+++ src/main/kotlin/com/norton/msit/event_management/auth/UserRepository.kt
@@ -7,6 +7,7 @@ import java.util.*
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
 
+
     override fun findAll() : MutableList<User>
 
     fun findFirstByTelegramId(terminalId: String?) : Optional<User>
